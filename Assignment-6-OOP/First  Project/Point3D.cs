540ce4bc11cd749401ef2eff34f3524669491694
HazemEltawel <eltawelhazem5@gmail.com>
2025-07-25 15:49:20 +0300

    Add project files.

---
+++ Assignment-6-OOP/First  Project/Point3D.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6_OOP.First__Project
+{
+    public class Point3D : IComparable<Point3D>, IComparable
+    {
+
+        public int x { get; set; }
+        public int y { get; set; }
+        public int z { get; set; }
+        public Point3D() : this(0, 0, 0) { }
+        public Point3D(int value) : this(value, value, value) { }
+        public Point3D(int X, int Y, int Z)
+        {
+            x = X;
+            y = Y;
+            z = Z;
+        }
+        public override string ToString()
+        {
+            return $"({x}, {y}, {z})";
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj is Point3D other)
+            {
+                return x == other.x && y == other.y && z == other.z;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(x, y, z);
+        }
+        public static bool operator ==(Point3D left, Point3D right)
+        {
+            return left.Equals(right);
+        }
+        public static bool operator !=(Point3D left, Point3D right)
+        {
+            return !(left == right);
+        }
+
+
+        public int CompareTo(Point3D? other)
+        {
+            if (other == null) return 1; 
+            int result = x.CompareTo(other.x);
+            if (result != 0) return result;
+            result = y.CompareTo(other.y);
+            if (result != 0) return result;
+            return z.CompareTo(other.z);
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj == null) return 1;
+            if (obj is Point3D other)
+            {
+                return CompareTo(other);
+            }
+            throw new ArgumentException("Object is not a Point3D");
+        }
+        public object Clone()
+        {
+            return new Point3D(x, y, z);
+        }
+    }
+}
---
+++ Experince-6-OOP/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Assignment_6_OOP.First__Project;
+class program
+{
+    static void Main(string[] args)
+    {
+        Point3D p = new Point3D(10, 10, 10);
+        Console.WriteLine(p.ToString());
+
+       
+        Point3D p1 = ReadPointFromUser("P1");
+        Point3D p2 = ReadPointFromUser("P2");
+
+       
+        if (p1 == p2)
+            Console.WriteLine("P1 and P2 are equal.");
+        else
+            Console.WriteLine("P1 and P2 are NOT equal.");
+
+     
+        Point3D[] points = new Point3D[]
+        {
+            new Point3D(2, 3, 1),
+            new Point3D(1, 5, 9),
+            new Point3D(2, 1, 0),
+            new Point3D(0, 0, 0)
+        };
+
+        Array.Sort(points); 
+
+        Console.WriteLine("\nSorted Points by X then Y:");
+        foreach (Point3D point in points)
+        {
+            Console.WriteLine(point);
+        }
+
+       
+        Point3D clone = (Point3D)p1.Clone();
+        Console.WriteLine($"\nCloned P1: {clone}");
+    }
+
+    static Point3D ReadPointFromUser(string name)
+    {
+        int x = ReadInt($"{name} - Enter X: ");
+        int y = ReadInt($"{name} - Enter Y: ");
+        int z = ReadInt($"{name} - Enter Z: ");
+        return new Point3D(x, y, z);
+    }
+
+    
+    static int ReadInt(string message)
+    {
+        int value;
+        Console.Write(message);
+        while (!int.TryParse(Console.ReadLine(), out value))
+        {
+            Console.Write("Invalid input. Try again: ");
+        }
+        return value;
+    }
+
+
+} 
+
